f587732a8bafa2405199e806512afa031c5a62e7
31469371 <31469371@087828-P33S105.macklabs.br>
2015-09-23 10:33:03 -0300

    Ticket machine
    - Correção do iterador no metodo inserir
    
    - correção do metodo getTroco
    
    
    Troco
    
    - Correcao do metodo hasNext
    - Correcao do metodo next

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -22,7 +22,7 @@ public class TicketMachine {
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
+            if (papelMoeda[i] == quantia) {
                 achou = true;
             }
         }
@@ -36,8 +36,9 @@ public class TicketMachine {
         return saldo;
     }
 
-    public Iterator<Integer> getTroco() {
-        return null;
+    public Iterator<PapelMoeda> getTroco() {
+        Troco troco = new Troco(saldo - valor);
+        return troco.getIterator();
     }
 
     public String imprimir() throws SaldoInsuficienteException {
